4e36c4467016d2bbe7627df7e35c25c1a46cf603
lucasns06 <2006lucasns@gmail.com>
2024-10-18 22:39:21 -0300

    Api de clima no Maui

---
+++ ClimaTempo/Models/Previsao.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ClimaTempo.Models
+{
+    public class Previsao
+    {
+        public string Cidade { get; set; }
+        public string Estado { get; set; }
+        public DateTime Atualizado_em{ get; set; }
+        public List<Clima> clima { get; set; }
+    }
+}
---
+++ ClimaTempo/ViewModels/PrevisaoViewModel.cs
@@ -0,0 +1,31 @@
+﻿using ClimaTempo.Models;
+using ClimaTempo.Services;
+using CommunityToolkit.Mvvm.ComponentModel;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Input;
+
+namespace ClimaTempo.ViewModels
+{ //parcial porque precisa de outro arquivo para funcionar
+    public partial class PrevisaoViewModel : ObservableObject
+    {
+        [ObservableProperty]
+        private string cidade;
+        private Previsao previsao;
+        public ICommand BuscarPrevisaoCommand {  get; }
+        public PrevisaoViewModel() 
+        {
+            BuscarPrevisaoCommand = new Command(BuscarPrevisao);
+        }
+
+        public async void BuscarPrevisao()
+        {
+            previsao = await new PrevisaoService().GetPrevisaoById(244);
+            Cidade = previsao.Cidade;
+        }
+    
+    }
+}
